12e8c6d191c3e07028df3afe29521a627dd04c1b
zaibali <zaibali51@live.com>
2024-07-28 23:35:01 +0500

    nested ul

--- src/app/posts.tsx
+++ src/app/posts.tsx
@@ -8,12 +8,12 @@ export default function Posts({posts}:PostsProps){
   console.log(posts);
   return (
     <>
-      {posts?.map((post: any)=>(
+      {posts?.map(({content, likes} : PostType)=>(
         <ul>
             <h3>Content:</h3>
-            <li>{post.content}</li>
+            <ul><li>{content}</li></ul>
             <h3>likes:</h3>
-            <li>{post.likes}</li>
+            <ul><li>{likes}</li></ul>
         </ul>
       ))}
     </>
